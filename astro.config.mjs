8ca25c82f257eaf7854e8ccf47a911c6a8147996
patricio0312rev <juan.marroquin1@unmsm.edu.pe>
2025-12-16 23:27:21 -0500

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   site: 'https://patriciomarroquin.dev',
@@ -12,13 +11,4 @@ export default defineConfig({
     sitemap(),
   ],
   output: 'static',
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-    speedInsights: { enabled: true },
-  }),
-  vite: {
-    ssr: {
-      noExternal: ['swiper'],
-    },
-  },
 });
